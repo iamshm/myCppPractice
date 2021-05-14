ff1131b6258bd604624de028b2d1146e8497bf34
iamshm <krynx9.9@gmail.com>
2021-05-14 22:00:52 +0530

    Flip to make alternate

---
+++ 450qstns/74.cpp
@@ -0,0 +1,83 @@
+//No of Flips to get alternating binary string
+//https://practice.geeksforgeeks.org/problems/min-number-of-flips3210/1
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define endl '\n'
+#define inf INT_MAX
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+#define vi vector<ll>
+const int MOD = 1e9 + 7;
+#define loop(i,a,b) for(int i =(a);i<=(b);i++)
+#define looprev(i,a,b) for(int i = (a);i>=b;i--)
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+/*-------------------------------------------------------------------------------------*/
+int flipCount(string s, bool expected){
+    int flips = 0;
+    for(int i=0;i < s.size();i++){
+        if((s[i]-'0') != expected){
+            flips++;
+        }
+        expected^=1; //flips the expected val to 0 or 1 accordingly
+    }
+    return flips;
+}
+void solve(){
+    string s;
+    cin >> s;
+    int startWithZero = flipCount(s,0);
+    int startWithOne = flipCount(s,1);
+    cout << min(startWithOne,startWithZero) << endl;
+}
+
+// with a given string of 0 n 1 there can be only 2 alternating strings
+// for 11001010
+// 10101010 and 01010101
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("/mnt/d/cpp/Error.txt", "w", stderr);
+#endif
+    fastio();
+    clock_t begin = clock();
+
+    int t = 1;
+    //cin >> t;
+    while(t--){
+        solve();
+    }
+
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 1000 << " ms";
+#endif
+    return 0;
+}
+
